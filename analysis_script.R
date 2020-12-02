8fa7028b5321153f909d7bee8b0dcc28892c6019
edzwick <erin.zwick@northwestern.edu>
2020-12-02 15:29:47 -0600

    added GF module visuals

--- analysis_script.R
+++ analysis_script.R
@@ -1,8 +1,11 @@
-setwd('C:/Users/edz9905/Box/erin-work/bf_costing_optimization')
+setwd('C:/Users/edz9905/Box/NU-malaria-team/projects/hbhi_burkina/bf_costing_optimization/scripts')
+working_dir <- 'C:/Users/edz9905/Box/NU-malaria-team/projects/hbhi_burkina/bf_costing_optimization'
 library(ggplot2)
 #install.packages("tidyverse")
 library(tidyverse)
 library(viridis)
+library(cowplot)
+library(stringr)
 theme_set(theme_bw())
 
 ####  load data ####
@@ -162,4 +165,139 @@ drug_unit_costs
 dev.off()
 
 drug_costs$`Row title`
+
+
+#### look at BF costing categories  ####
+full_budget.df <- read_csv(sprintf('%s/BF_costing_data/cleaned_data/full_detailed_budget.csv', working_dir))
+full_budget.df$`Global Fund Modules` <- as.factor(full_budget.df$`Global Fund Modules`)
+full_budget.df$`Global Fund interventions` <- as.factor(full_budget.df$`Global Fund interventions`)
+temp <- which(full_budget.df$`Global Fund Cost Categories` == '2.0 Travel Costs (Travel)')
+full_budget.df[temp,]$`Global Fund Cost Categories` <- '2.0 Travel costs (Travel)'
+full_budget.df$`Cost 2021 (CFA)` <- as.numeric(full_budget.df$`Cost 2021 (CFA)`)
+
+module_costs <- ggplot(full_budget.df %>% filter(Level == "Sub-activity"), 
+       aes(`Global Fund Modules`, `Cost 2021 (CFA)`, fill = `Global Fund Cost Categories`)) +
+  geom_bar(stat = 'identity', position = "stack") +
+  scale_fill_viridis(discrete = T, option = 'plasma', na.value = "grey") +
+  labs(title = "2021 Global Fund module costs by cost category", y = "Cost (2021 CFA)", 
+       fill = "Cost Category", x = "") +
+  theme(axis.text.x = element_text(angle = 40, hjust=1), legend.position = 'bottom') +
+  guides(fill=guide_legend(nrow=4,byrow=TRUE, title.position="top", title.hjust = 0.5))
+
+pdf("output/figures/GF_module_costs.pdf", height = 11, width = 8.5)
+module_costs
+dev.off()
+
+module_interventions <- ggplot(full_budget.df %>% filter(Level == "Sub-activity"), 
+                       aes(`Global Fund Modules`, `Cost 2021 (CFA)`, fill = `Global Fund interventions`)) +
+  geom_bar(stat = 'identity', position = "stack") +
+  scale_fill_viridis(discrete = T, option = 'plasma', na.value = "grey") +
+  labs(title = "2021 Global Fund module costs by intervention category", y = "Cost (2021 CFA)", 
+       fill = "Intervention Category", x = "") +
+  theme(axis.text.x = element_text(angle = 40, hjust=1), legend.position = 'bottom',
+        legend.key.size = unit(0.2, "cm"), legend.box="vertical", legend.margin=margin(), 
+        legend.text = element_text(size = 5)) +
+  guides(fill=guide_legend(ncol=2,byrow=F, title.position="top", title.hjust = 0.5))
+module_interventions
+
+pdf("output/figures/GF_module_interventions.pdf", height = 11, width = 8.5)
+module_interventions
+dev.off()
+
+pc_module_interventions <- ggplot(full_budget.df %>% filter(Level == "Sub-activity"), 
+                               aes(`Global Fund Modules`, `Cost 2021 (CFA)`, fill = `Global Fund interventions`)) +
+  geom_bar(stat = 'identity', position = "fill") +
+  scale_fill_viridis(discrete = T, option = 'plasma', na.value = "grey") +
+  labs(title = "2021 Global Fund module costs by intervention category", y = "Percent of total module cost", 
+       fill = "Intervention Category", x = "") +
+  theme(axis.text.x = element_text(angle = 40, hjust=1), legend.position = 'bottom',
+        legend.key.size = unit(0.2, "cm"), legend.box="vertical", legend.margin=margin(), 
+        legend.text = element_text(size = 5)) +
+  guides(fill=guide_legend(ncol=2,byrow=F, title.position="top", title.hjust = 0.5))
+pc_module_interventions
+
+pdf("output/figures/GF_module_interventions_percent.pdf", height = 11, width = 8.5)
+pc_module_interventions
+dev.off()
+
+
+intervention_costs <- ggplot(full_budget.df %>% filter(Level == "Sub-activity"), 
+                       aes(`Global Fund interventions`, `Cost 2021 (CFA)`, fill = `Global Fund Cost Categories`)) +
+  geom_bar(stat = 'identity', position = "stack") +
+  scale_fill_viridis(discrete = T, option = 'plasma') +
+  labs(title = "2021 Global Fund intervention costs by cost category", y = "Cost (2021 CFA)", 
+       fill = "Cost Category", x = "") +
+  theme(axis.text.x = element_text(angle = 90, hjust=1), legend.position = 'bottom',
+        legend.key.size = unit(0.2, "cm"), legend.box="vertical", legend.margin=margin(), 
+        legend.text = element_text(size = 7)) +
+  guides(fill=guide_legend(nrow=4,byrow=TRUE, title.position="top", title.hjust = 0.5))
+intervention_costs
+
+pdf("output/figures/GF_intervention_costs.pdf", height = 11, width = 8.5)
+intervention_costs
+dev.off()
+
+view(full_budget.df %>% 
+       filter(`Global Fund Modules` == "Specific prevention interventions"))
+
+unique(full_budget.df %>% 
+         filter(`Global Fund Modules` == "Specific prevention interventions")$`Global Fund interventions`)
+
+
+cm_interventions <- ggplot(full_budget.df %>% filter(Level == "Sub-activity") %>% 
+                                 filter(`Global Fund Modules` == "Case management"), 
+                               aes(`Global Fund Modules`, `Cost 2021 (CFA)`, fill = `Global Fund interventions`)) +
+  geom_bar(stat = 'identity', position = "fill") +
+  scale_fill_viridis(discrete = T, option = 'plasma', na.value = "grey") +
+  labs(title = "2021 Case management costs by intervention category", y = "% of cost", 
+       fill = "Intervention Category", x = "") +
+  theme(axis.text.x = element_text(angle = 40, hjust=1), legend.position = 'bottom',
+        legend.key.size = unit(0.2, "cm"), legend.box="vertical", legend.margin=margin(), 
+        legend.text = element_text(size = 5)) +
+  guides(fill=guide_legend(ncol=2,byrow=F, title.position="top", title.hjust = 0.5))
+cm_interventions
+
+pdf("output/figures/GF_module_interventions.pdf", height = 11, width = 8.5)
+cm_interventions
+dev.off()
+
+# separate modules for easier category viewing
+percentage_bar <- function(df, filter_var, x_var, y_var, fill_var, x_label, y_label, fill_label) 
+{
+  ggplot(df, aes_string(x = x_var, y = y_var, fill = fill_var)) +
+    geom_bar(stat = 'identity', position = "fill") +
+    scale_fill_viridis(discrete = T, option = 'plasma', na.value = "grey") +
+    labs(title = str_wrap(filter_var, 40),
+         y = y_label, fill = fill_label, x = x_label) +
+    theme(legend.position = 'bottom',legend.key.size = unit(0.2, "cm"), legend.box="vertical", 
+          legend.margin=margin(), legend.text = element_text(size = 5),
+          legend.title=element_text(size=10), plot.title = element_text(hjust = 0.5),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank()) +
+    guides(fill=guide_legend(ncol=1,byrow=F, title.position="top", title.hjust = 0.5))
+}
+
+modules <- unique(subset(full_budget.df, full_budget.df$Level == "Sub-activity")$`Global Fund Modules`)
+modules <- as.character(modules)
+
+plot_list <- list()
+
+for(i in 1:length(modules))
+{
+  temp_plot <- percentage_bar(full_budget.df %>% filter(Level == "Sub-activity") %>% 
+                                filter(`Global Fund Modules` == modules[i]),
+                              modules[i], 
+                              "`Global Fund Modules`", "`Cost 2021 (CFA)`", "`Global Fund interventions`",
+                              "", "% of cost", "Intervention category")
+  plot_list[[i]] <- temp_plot
+}
+
+separate_modules <- plot_grid(plotlist = plot_list[c(9,10,11)])
+separate_modules
+
+
+pdf("output/figures/GF_module_interventions_separate3.pdf", height = 11, width = 8.5)
+separate_modules
+dev.off()
+
 
